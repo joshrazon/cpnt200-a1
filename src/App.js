9969eb79f01525a36fd8bd61bf1fb70219dfb817
Josh <joshrazonn@gmail.com>
2021-02-22 16:42:37 -0700

    created auth ui

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+Amplify.configure(config)
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!!!</h1>
-      </header>
-      <AmplifySignOut />
-    </div>
+    <AmplifyAuthenticator>
+      <div className="App">
+        <header>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>We now have Auth!!!</h1>
+        </header>
+        <AmplifySignOut />
+      </div>
+    </AmplifyAuthenticator>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
